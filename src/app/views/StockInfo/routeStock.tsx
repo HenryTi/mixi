e2c7aac713297aec105ff584867d41695cf4ee74
HenryTi <liaohengyi@gmail.com>
2023-02-27 10:40:15 -0500

    deploy

--- src/app/views/StockInfo/routeStock.tsx
+++ src/app/views/StockInfo/routeStock.tsx
@@ -3,6 +3,7 @@ import { StoreStockInfo } from "app/stores";
 import { PageBonusDetail } from "./PageBonusDetail";
 import { PageProfitDetail } from "./PageProfitDetail";
 import { PageStockInfo } from "./PageStockInfo";
+import { useMemo, useRef } from "react";
 
 export function pathStockInfo(id: string | number): string {
     return `/stock/${id}`;
@@ -19,5 +20,6 @@ export const routeStock = <Route path={pathStockInfo(':id')} element={<LayoutSto
 
 function LayoutStockInfo() {
     const { id } = useParams();
-    return <Outlet context={new StoreStockInfo(Number(id))} />;
+    let ssi = useMemo(() => new StoreStockInfo(Number(id)), [id]);
+    return <Outlet context={ssi} />;
 }
